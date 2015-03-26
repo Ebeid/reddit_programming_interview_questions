e074a2c9461186fc1d57954c8b344bc8491f26da
dlee0113 <dlee0113@gmail.com>
2015-03-26 14:05:54 -0400

    initial import

---
+++ reddit_programming_interview_questions/src/main/java/strings/CheckForUnique.java
@@ -0,0 +1,27 @@
+package strings;
+
+// http://stackoverflow.com/questions/19484406/detecting-if-a-string-has-unique-characters-comparing-my-solution-to-cracking
+public class CheckForUnique {
+	public static void main(String[] args) {
+		System.out.println(hasDuplicates("aac"));
+		System.out.println(hasDuplicates("abc"));
+	}
+
+	public static boolean hasDuplicates(String word) {
+		boolean[] check = new boolean[26]; // 26 English alphabets
+		
+		for (int i = 0; i < word.length(); i++) {
+			// int value of 'a' is 97. By subtracting this number we find array
+			// index
+			// starting from 0 to 26
+			int pos = word.toLowerCase().charAt(i) - 'a';
+			
+			if (check[pos])
+				return true;
+			else
+				check[pos] = true;
+		}
+		
+		return false;
+	}
+}
