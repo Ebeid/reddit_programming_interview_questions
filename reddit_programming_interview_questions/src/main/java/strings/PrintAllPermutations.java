8eb9ba53d2fde75d163e704b6f91a1456da22c51
dlee0113 <dlee0113@gmail.com>
2015-03-27 11:26:11 -0400

    added print statements

--- reddit_programming_interview_questions/src/main/java/strings/PrintAllPermutations.java
+++ reddit_programming_interview_questions/src/main/java/strings/PrintAllPermutations.java
@@ -4,12 +4,18 @@ package strings;
 public class PrintAllPermutations {
 	private static void permutation(String prefix, String str) {
 		int n = str.length();
+		
+		System.out.print("prefix: " + prefix + " str: " + str + " n: " + n + " ");
+
 		if (n == 0)
 			System.out.println(prefix);
 		else {
-			for (int i = 0; i < n; i++)
+			for (int i = 0; i < n; i++) {
+				System.out.println("i: " + i + " character: " + str.charAt(i));
+				
 				permutation(prefix + str.charAt(i),
 						str.substring(0, i) + str.substring(i + 1));
+			}
 		}
 	}
 
